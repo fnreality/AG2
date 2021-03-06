e2f21a7e163d138eab8f99588c8be0fdcbbd1c6e
fnreality <59921084+fnreality@users.noreply.github.com>
2021-03-06 15:50:23 -0500

    begin 2 about to shift to class

--- AG2.scala
+++ AG2.scala
@@ -2,14 +2,14 @@ import scala.language.postfixOps
 
 // Represent the programs environment
 case class Env(
-    val line: Int,                            // The current line the program is on, akin to the instruction pointer
-    val stack_refs: List[String],             // The stack, as named references to the original sources, (moves handled)
-    val cs: List[Int],                        // The call stack, containing the return addresses as line numbers
-    val cmp_state: ComparatorState,           // The current state of the internal comparator, manipulated with 'cmp
-    val op_sources: Map[String, Symbol],      // Associations from registers to the operation that created them
-    val dyn_loc_sources: List[Symbol],        // The dynamic operations that lead to the current location of line
-    val mov_sources: Map[String, String],     // Associations from registers to their original source their data is from
-    val linked: Set[Tuple2[Symbol, Symbol]]   // Which operations are dependent on each other, their part of the result
+  val line: Int,                            // The current line the program is on, akin to the instruction pointer
+  val stack_refs: List[String],             // The stack, as named references to the original sources, (moves handled)
+  val cs: List[Int],                        // The call stack, containing the return addresses as line numbers
+  val cmp_uses: Tuple2[Symbol, Symbol],     // The current targets of the internal comparator, manipulated with 'cmp
+  val op_sources: Map[String, Symbol],      // Associations from registers to the operation that created them
+  val dyn_loc_sources: List[Symbol],        // The dynamic operations that lead to the current location of line
+  val mov_sources: Map[String, String],     // Associations from registers to their original source their data is from
+  val linked: Set[Tuple2[Symbol, Symbol]]   // Which operations are dependent on each other, their part of the result
 )
 
 // A trait representing an instruction that can be executed in this context
@@ -26,20 +26,53 @@ case class StandardOp(op: Symbol, gen: List[String], uses: List[String])    exte
 
 // A no-op, like 'nop or 'hint_nop7
 case class NoOp()                                                           extends Instruction {
-  def eval(env: Env): Env = env
+  def eval(env: Env): Env = env                                             // Do nothing (the identity function)
 }
 
 // An unconditional jump, like 'jmp
 case class UnconditionalJump(target: Int)                                   extends Instruction {
   def eval(env: Env): Env = Env(
-    target,
-    env.stack_refs, env.cs, env.cmp_state, env.op_sources, env.dyn_loc_sources, env.mov_sources,
-    env.linked
+    target,                                                                 // Overwrite the instruction pointer
+    env.stack_refs, env.cs, env.cmp_uses,                                   // Preserve everything else about the system
+    env.op_sources, env.dyn_loc_sources, env.mov_sources,                   // Including our interpretation of it
+    env.linked                                                              // Including the intermediate result
   )
 }
 
 // A conditional jump, like 'jnz
 case class ConditionalJump(op: Symbol, target: Int)                         extends Instruction {
-  def eval(env: Env): Env = Env
+  def eval(env: Env): Env = {
+    ???
+  }
+}
+
+// A call to a procedure, like 'call
+case class ProcedureCall(target: Int)                                       extends Instruction {
+  def eval(env: Env): Env = {
+    val return_addr = env.line + 1                                          // Calculate the return address
+    return Env(
+      target,                                                               // Overwrite the instruction pointer
+      env.stack_refs,                                                       // Preserve the stack
+      return_addr :: env.cs,                                                // But update the virtual call stack
+      env.cmp_uses, env.op_sources, env.dyn_loc_sources, env.mov_sources,   // Preserve everything else
+      env.linked                                                            // Including the intermediate result
+    )
+  }
+}
+
+// A return from a procedures, like 'ret
+case class ProcedureReturn()                                                extends Instruction {
+  def eval(env: Env): Env = env.cs match {
+    case return_addr :: cs_remaining                                        // If the call stack has a return address
+      => Env(
+        return_addr,                                                        // Jump to the return address
+        env.stack_refs,                                                     // Preserve the stack
+        cs_remaining,                                                       // Return what's remaining to the call stack
+        env.cmp_uses, env.op_sources, env.dyn_loc_sources, env.mov_sources, // Preserve everything else
+        env.linked                                                          // Including the intermediate result
+      )
+    case _                                                                  // Otherwise, if the call stack is empty
+      => throw new IllegalArgumentException("Call Stack Underflow")         // Error out, this is not allowed
+  }
 }
 
