23e6bec74d9fb43bdc3bc08c742e793f62bd0bf9
fnreality <59921084+fnreality@users.noreply.github.com>
2021-03-07 22:04:09 -0500

    Update AG2.scala

--- AG2.scala
+++ AG2.scala
@@ -1,35 +1,21 @@
-import scala.util.NotGiven
+import scala.language.implicitConversions
 
-// Represent these programs environments
-class Env(
-  val line: Int,                            // The current line the program is on, akin to the instruction pointer
-  val stack_refs: List[String],             // The stack, as named references to the original sources, (moves handled)
-  val cs: List[Int],                        // The call stack, containing the return addresses as line numbers
-  val cmp_uses: Tuple2[Symbol, Symbol],     // The current targets of the internal comparator, manipulated with 'cmp
-  val op_sources: Map[String, Symbol],      // Associations from registers to the operation that created them
-  val dyn_loc_sources: List[Symbol],        // The dynamic operations that lead to the current location of line
-  val mov_sources: Map[String, String],     // Associations from registers to their original source their data is from
-  val linked: Set[Tuple2[Symbol, Symbol]]   // Which operations are dependent on each other, their part of the result
-)
+trait GEnv[+T] {
+  def empty: T
+}
 
-trait Interpretable[T, E, V]:
-  def eval(expr: T, env: E): Tuple2[V, E]
+given GEnv[Int] with
+  def empty = 0
 
-class StandardOp(op: Symbol, gen: [String], uses: [String])     // A normal operation, like 'dec
-class NoOp                                                      // A no-op, 'nop or 'hint_nop7
+case class OptTargetEnv[+E : GEnv, +V](env: E, value: Option[V])
 
+given [E : GEnv, V]: Conversion[OptTargetEnv[E, V], E] = _.env
 
-given Interpretable[StandardOp, Env, Unit] with
-  def eval(expr: StandardOp, env: Env) =
-    ???
-  end eval
+given [E : GEnv]: Conversion[E, OptTargetEnv[E, Nothing]] = OptTargetEnv(_, None)
 
-given Interpretable[NoOp, Env, Unit] with
-  def eval(expr: NoOp, env: Env) =
-    () -> env
-  end eval
+def main(args: Array[String]) =
+  val z: OptTargetEnv[Int, Nothing] = OptTargetEnv.Without(Env(7))
+  val xprocess: Env => Int = a => a.x
+  println(((a: GEnv) => xprocess(a) + 1)(z))
+end main
 
-given [T](using NotGiven[Interpretable[T, Unit, T]]) with
-  def eval(expr: T, env: Unit) =
-    expr -> ()
-  end eval
